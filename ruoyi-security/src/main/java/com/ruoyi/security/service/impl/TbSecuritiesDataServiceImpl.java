461ad330f0c9a03c8e84c1cb632725bc15800f97
linhf3 <191663059@qq.com>
2025-06-30 08:11:40 +0800

    更新

--- ruoyi-security/src/main/java/com/ruoyi/security/service/impl/TbSecuritiesDataServiceImpl.java
+++ ruoyi-security/src/main/java/com/ruoyi/security/service/impl/TbSecuritiesDataServiceImpl.java
@@ -1,9 +1,12 @@
 package com.ruoyi.security.service.impl;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 import com.alibaba.fastjson2.JSON;
@@ -63,6 +66,8 @@ public class TbSecuritiesDataServiceImpl implements ITbSecuritiesDataService
 
     @Autowired
     private ITbSecuritiesHistoryService iTbSecuritiesHistoryService;
+
+    ReentrantLock lock = new ReentrantLock();
 
     /**
      * 查询证劵交易
@@ -449,8 +454,70 @@ public class TbSecuritiesDataServiceImpl implements ITbSecuritiesDataService
     }
 
     @Override
-    public void startMonitoringStrategy1() {
+    public void logSina15() throws InterruptedException {
+        boolean b = lock.tryLock();
+        if (!b){
+            return;
+        }
+        try {
+            // 获取当前时间
+            LocalTime now = LocalTime.now();
+            //判断是否是周末
+            DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+//            if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY){
+//                return;
+//            }
+            // 定义目标时间 11:35
+            LocalTime targetTime1 = LocalTime.of(9, 00);
+            LocalTime targetTime2 = LocalTime.of(11, 31);
+            LocalTime targetTime3 = LocalTime.of(13, 30);
+            LocalTime targetTime4 = LocalTime.of(15, 01);
+            LocalTime targetTime5 = LocalTime.of(21, 00);
+            LocalTime targetTime6 = LocalTime.of(23, 00);
+            if ((now.isAfter(targetTime1) && now.isBefore(targetTime2)) || (now.isAfter(targetTime3) && now.isBefore(targetTime4))
+                    || (now.isAfter(targetTime5) && now.isBefore(targetTime6))){
+                List<TbSecuritiesData> tbSecuritiesDataList = redisCache.getCacheMapValue("money","f_sina_tbSecuritiesDataList");
+                if (CollectionUtils.isEmpty(tbSecuritiesDataList)){
+                    return;
+                }
+                long startTime = System.currentTimeMillis();
+                for (TbSecuritiesData tbSecuritiesData : tbSecuritiesDataList) {
+                    TbSecuritiesDataSinaThread tbSecuritiesDataThread = new TbSecuritiesDataSinaThread(tbSecuritiesData, coreAlgorithmContet, Constant.SINA_FIFTEEN_MIN_LINE);
+                    SecuritiesSinaFutureVo securitiesSinaFutureVo = tbSecuritiesDataThread.call();
+                    List<SecuritiesSinaFutureVo> securitiesSinaFutureVoList = redisCache.getCacheMapValue("money","securitiesSinaFutureVoList");
+                    if (CollectionUtils.isEmpty(securitiesSinaFutureVoList)){
+                        List<SecuritiesSinaFutureVo> list = new ArrayList<>();
+                        list.add(securitiesSinaFutureVo);
+                        redisCache.setCacheMapValue("money","securitiesSinaFutureVoList",list);
+                    }else {
+                        List<SecuritiesSinaFutureVo> collect = securitiesSinaFutureVoList.stream().filter(t -> t.getCode().equals(securitiesSinaFutureVo.getCode())).collect(Collectors.toList());
+                        if (!CollectionUtils.isEmpty(collect)){
+                            securitiesSinaFutureVoList.remove(collect.get(0));
+                        }
+                        securitiesSinaFutureVoList.add(securitiesSinaFutureVo);
+                        //按照振幅降序排序
+                        securitiesSinaFutureVoList.sort(Comparator.comparing(SecuritiesSinaFutureVo::getPositiveNegativeFlag));
+                        redisCache.setCacheMapValue("money","securitiesSinaFutureVoList",securitiesSinaFutureVoList);
+                    }
+                    if (1 == securitiesSinaFutureVo.getPositiveNegativeFlag() || 2 == securitiesSinaFutureVo.getPositiveNegativeFlag()){
 
+                    }
+                    //判断当前振幅是否大于等于缓存中的
+                    Thread.sleep(8000);
+                }
+                long endTime = System.currentTimeMillis();
+                log.debug("执行时长：{}", endTime - startTime);
+            }
+        }catch (Exception e){
+            log.info("异常：",e);
+        }finally {
+            if (b){
+                lock.unlock();
+                log.debug("logSina15 == 释放锁成功，休眠20秒");
+                Thread.sleep(2,000);
+                this.logSina15();
+            }
+        }
     }
 
 
